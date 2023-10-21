0252cd774c37a52bcfc9bd59239f88e7cce26689
jjg-git <john_joseph_giron@dlsu.edu.ph>
2023-10-21 19:06:50 +0800

    Added Movie.kt

---
+++ app/src/main/java/com/mobdeve/s15/gironasayasranario/cinereview/Movie.kt
@@ -0,0 +1,10 @@
+package com.mobdeve.s15.gironasayasranario.cineview
+
+class Movie (
+    private val name: String,
+    private val genre: String,
+    private val casts: ArrayList<String>,
+    private val directors: ArrayList<String>,
+    private val promoPics: ArrayList<String>,
+    private val rating: String
+)
