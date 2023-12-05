174f31f27bf13ee2c4a79e309b8bf1315755ebeb
jjg-git <john_joseph_giron@dlsu.edu.ph>
2023-12-06 00:04:57 +0800

    Added Navigation Fragments dependencies.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,11 +41,20 @@ android {
 }
 
 dependencies {
+    val nav_version = "2.7.5"
 
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+
+    // Kotlin
+    implementation("androidx.navigation:navigation-fragment-ktx:$nav_version")
+    implementation("androidx.navigation:navigation-ui-ktx:$nav_version")
+
+    // Feature module Support
+    implementation("androidx.navigation:navigation-dynamic-features-fragment:$nav_version")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
