bc788bc31dc954c6c0515550a37e5a1b59b30729
jjg-git <john_joseph_giron@dlsu.edu.ph>
2023-12-06 02:42:39 +0800

    Implemented DB Controller.

--- app/src/main/java/com/mobdeve/s15/gironasayasranario/cinereview/DBController.kt
+++ app/src/main/java/com/mobdeve/s15/gironasayasranario/cinereview/DBController.kt
@@ -1,7 +1,15 @@
 package com.mobdeve.s15.gironasayasranario.cinereview
 
+import com.google.android.gms.tasks.Task
+import com.google.firebase.Firebase
+import com.google.firebase.firestore.QuerySnapshot
+import com.google.firebase.firestore.firestore
+
 class DBController {
-//    companion object {
-////        private val db = Firebase
-//    }
+    companion object {
+        private val db = Firebase.firestore
+        fun getData(collection: String): Task<QuerySnapshot> {
+            return db.collection(collection).get()
+        }
+    }
 }
