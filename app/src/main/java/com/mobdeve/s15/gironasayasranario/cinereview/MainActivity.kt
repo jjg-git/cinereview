25ed2b3bfda3e4d1b41cc15c7219ea271f13e990
jjg-git <john_joseph_giron@dlsu.edu.ph>
2023-12-06 00:07:04 +0800

    Simplified MainActivity code.

--- app/src/main/java/com/mobdeve/s15/gironasayasranario/cinereview/MainActivity.kt
+++ app/src/main/java/com/mobdeve/s15/gironasayasranario/cinereview/MainActivity.kt
@@ -15,30 +15,10 @@ import com.mobdeve.s15.gironasayasranario.cineview.Movie
 class MainActivity : AppCompatActivity() {
     private lateinit var binding:ActivityMainBinding
 
-    private lateinit var data: ArrayList<Movie>
-    private lateinit var movieRecycler: RecyclerView
-    private lateinit var movieLayout: LinearLayoutManager
-
-    private val movieLauncher =
-        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
-
-        movieRecycler = binding.movieRv
-
-        this.data = DataGenerator.generateData()
-        this.movieLayout = LinearLayoutManager(this,
-                LinearLayoutManager.HORIZONTAL, false)
-
-        movieRecycler.layoutManager = movieLayout
-        movieRecycler.adapter = MovieAdapter(data, movieLauncher)
-
-
-
     }
 }
