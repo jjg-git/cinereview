0d524ce928c5029d27da871abc5377550864274c
jjg-git <john_joseph_giron@dlsu.edu.ph>
2023-12-06 04:27:54 +0800

    CRUD operations

--- app/src/main/java/com/mobdeve/s15/gironasayasranario/cinereview/DBController.kt
+++ app/src/main/java/com/mobdeve/s15/gironasayasranario/cinereview/DBController.kt
@@ -2,6 +2,8 @@ package com.mobdeve.s15.gironasayasranario.cinereview
 
 import com.google.android.gms.tasks.Task
 import com.google.firebase.Firebase
+import com.google.firebase.firestore.DocumentReference
+import com.google.firebase.firestore.LoadBundleTaskProgress
 import com.google.firebase.firestore.QuerySnapshot
 import com.google.firebase.firestore.firestore
 
@@ -10,6 +12,18 @@ class DBController {
         private val db = Firebase.firestore
         fun getData(collection: String): Task<QuerySnapshot> {
             return db.collection(collection).get()
+        }
+
+        fun insertData(collection: String, data:Any): Task<DocumentReference> {
+            return db.collection(collection).add(data)
+        }
+
+        fun updateData(collection: String, doc: String, newData: Map<String, Any>): Task<Void> {
+            return db.collection(collection).document(doc).update(newData)
+        }
+
+        fun deleteData(collection: String, doc: String): Task<Void> {
+            return db.collection(collection).document(doc).delete()
         }
     }
 }
