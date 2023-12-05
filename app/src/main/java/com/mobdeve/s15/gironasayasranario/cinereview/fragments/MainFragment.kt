e8778f3387ed33923e922cf657762b7843d5632a
jjg-git <john_joseph_giron@dlsu.edu.ph>
2023-12-06 00:04:17 +0800

    Replaced the UI into Fragments and used Navigation instead.

--- app/src/main/java/com/mobdeve/s15/gironasayasranario/cinereview/fragments/MainFragment.kt
+++ app/src/main/java/com/mobdeve/s15/gironasayasranario/cinereview/fragments/MainFragment.kt
@@ -1,10 +1,18 @@
-package com.mobdeve.s15.gironasayasranario.cinereview
+package com.mobdeve.s15.gironasayasranario.cinereview.fragments
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.mobdeve.s15.gironasayasranario.cinereview.DataGenerator
+import com.mobdeve.s15.gironasayasranario.cinereview.MovieAdapter
+import com.mobdeve.s15.gironasayasranario.cinereview.R
+import com.mobdeve.s15.gironasayasranario.cinereview.databinding.FragmentMainBinding
+import com.mobdeve.s15.gironasayasranario.cineview.Movie
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -13,13 +21,16 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [Scheduling.newInstance] factory method to
+ * Use the [MainFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class Scheduling : Fragment(R.layout.fragment_scheduling) {
+class MainFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+
+    private lateinit var data: ArrayList<Movie>
+    private lateinit var movieRecycler: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,6 +40,21 @@ class Scheduling : Fragment(R.layout.fragment_scheduling) {
         }
     }
 
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding = FragmentMainBinding.inflate(inflater)
+        val view = binding.root
+
+        movieRecycler = view.findViewById(R.id.movieRv)
+
+        this.data = DataGenerator.generateData()
+        movieRecycler.adapter = MovieAdapter(data, findNavController())
+
+        return view
+    }
+
     companion object {
         /**
          * Use this factory method to create a new instance of
@@ -36,12 +62,12 @@ class Scheduling : Fragment(R.layout.fragment_scheduling) {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment Scheduling.
+         * @return A new instance of fragment MainFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            Scheduling().apply {
+            MainFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
