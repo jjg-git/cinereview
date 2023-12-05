471439b48a5783c3ec3b7335d5cb61d8eb7c5435
jjg-git <john_joseph_giron@dlsu.edu.ph>
2023-12-06 00:07:42 +0800

    Added Click listener on every view holder.

--- app/src/main/java/com/mobdeve/s15/gironasayasranario/cinereview/MovieAdapter.kt
+++ app/src/main/java/com/mobdeve/s15/gironasayasranario/cinereview/MovieAdapter.kt
@@ -1,30 +1,54 @@
 package com.mobdeve.s15.gironasayasranario.cinereview
 
 import android.content.Intent
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.activity.result.ActivityResultLauncher
+import androidx.core.os.bundleOf
+import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.Adapter
+import com.mobdeve.s15.gironasayasranario.cinereview.databinding.FragmentMainBinding
 import com.mobdeve.s15.gironasayasranario.cinereview.databinding.MovieSelectBinding
 import com.mobdeve.s15.gironasayasranario.cineview.Movie
 
-class MovieAdapter(val data: ArrayList<Movie>, val movieActivityResultLauncher: ActivityResultLauncher<Intent>) : Adapter<MovieHolder>() {
+class MovieAdapter(val data: ArrayList<Movie>, val navController: NavController) : Adapter<MovieHolder>() {
+    private val TAG = "MovieAdapter"
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieHolder {
         val itemViewBinding: MovieSelectBinding = MovieSelectBinding.inflate(
             LayoutInflater.from(parent.context),
             parent,
             false
         )
-        return MovieHolder(itemViewBinding)
+        val holder = MovieHolder(itemViewBinding)
+        return holder
     }
 
     /*
     *   TODO: Complete the onBindViewHolder logic.
     * */
     override fun onBindViewHolder(holder: MovieHolder, position: Int) {
-        holder.bindData(data.get(position))
+        val movie = data.get(position)
+        val bundle = bundleOf(
+            "name" to movie.name,
+            "description" to movie.description,
+            "cast" to movie.casts,
+            "director" to movie.directors,
+            "rating" to movie.rating,
+            "poster" to movie.thumbnail
+        )
+
+
+        holder.viewBind.root.setOnClickListener {
+            navController.navigate(R.id.movieDetailFrag, bundle)
+            Log.d(TAG, "${bundle}")
+        }
+
+        holder.bindData(movie)
     }
 
     /*
