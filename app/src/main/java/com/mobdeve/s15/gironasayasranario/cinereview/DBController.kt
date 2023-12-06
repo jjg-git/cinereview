733d51256cb522f6817ef4fb1a8f18913480f109
jjg-git <john_joseph_giron@dlsu.edu.ph>
2023-12-06 11:23:25 +0800

    Added more DB functionality

--- app/src/main/java/com/mobdeve/s15/gironasayasranario/cinereview/DBController.kt
+++ app/src/main/java/com/mobdeve/s15/gironasayasranario/cinereview/DBController.kt
@@ -2,8 +2,8 @@ package com.mobdeve.s15.gironasayasranario.cinereview
 
 import com.google.android.gms.tasks.Task
 import com.google.firebase.Firebase
+import com.google.firebase.firestore.CollectionReference
 import com.google.firebase.firestore.DocumentReference
-import com.google.firebase.firestore.LoadBundleTaskProgress
 import com.google.firebase.firestore.QuerySnapshot
 import com.google.firebase.firestore.firestore
 
@@ -14,7 +14,15 @@ class DBController {
             return db.collection(collection).get()
         }
 
-        fun insertData(collection: String, data:Any): Task<DocumentReference> {
+        fun getDataWhere(collection: String, field: String, value:Any): Task<QuerySnapshot> {
+            return db.collection(collection).whereEqualTo(field, value).get()
+        }
+
+        fun getDocument(collection: String): CollectionReference {
+            return db.collection(collection)
+        }
+
+        fun insertData(collection: String, data: HashMap<String, Any?>): Task<DocumentReference> {
             return db.collection(collection).add(data)
         }
 
